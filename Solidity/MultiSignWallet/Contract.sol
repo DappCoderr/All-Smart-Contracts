0761318b0556766c158f60aa74eb74e143cc9019
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-01-18 16:35:16 +0530

    Update: optimizise MultiSign

--- Solidity/MultiSignWallet/Contract.sol
+++ Solidity/MultiSignWallet/Contract.sol
@@ -5,55 +5,66 @@ contract MultiSignWallet {
     event Deposit(address indexed sender, uint amount);
     event Submit(uint indexed txId);
     event Approve(address indexed owner, uint txId);
-    event Execute(uint indexed txId);
+    event ExecuteTransaction(address indexed sender, uint indexed txId);
     event Revoke(address indexed owner, uint indexed txId);
 
-    //holds the owner of this wallet
+    error MultiSignWallet__NotTheOwner();
+    error MultiSignWallet__TransactionExists();
+    error MultiSignWallet__TransactionAlreadyApproved();
+    error MultiSignWallet__TransactionAlreadyExecuted();
+    error MultiSignWallet__NoOwner();
+
     address[] public owners;
 
-    //Check the owner is in the mapping or not
-    mapping(address owner => bool ) public isOwner;
+    mapping(address => bool ) public isOwner;
 
-    //Require set the number of owner is required to approve the transactions
     uint required;
 
-    //struct to transtion that will be send
     struct Transaction{
         address to;
         uint value;
-        bytes32 data;
+        bytes data;
         bool executed;
     }
 
     modifier onlyOwner(){
-        require(isOwner[msg.sender], "Not the owner");
+        if(!isOwner[msg.sender]){
+            revert MultiSignWallet__NotTheOwner();
+        }
         _;
     }
 
     modifier txnExists(uint txId){
-        require(txId < transactions.length, "tx does not exit");
+        if(txId < transactions.length){
+            revert MultiSignWallet__TransactionExists();
+        }
         _;
     }
 
     modifier notExecuted(uint txId){
-        require(!transactions[txId].executed, "tx already executed");
+        if(!transactions[txId].executed){
+            revert MultiSignWallet__TransactionAlreadyExecuted();
+        }
         _;
     }
 
     modifier notApproved(uint txId){
-        require(!approve[txId][msg.sender], "tx already approved");
+        if(!approve[txId][msg.sender]){
+            revert MultiSignWallet__TransactionAlreadyApproved();
+        }
         _;
     }
 
-    //hold all the transaction struct info
     Transaction[] public transactions;
 
-    //matain the list of approvers of the transation
     mapping (uint => mapping(address => bool)) public approve;
 
-
     constructor(address[] memory _owners, uint _requred){
-        require(_owners.length > 0, "require owners");
+        require(_owners.length > 0, "no owner");
+
+        if(_owners.length > 0){
+            revert MultiSignWallet__NoOwner();
+        }
         require(_requred > 0 && _requred > _owners.length, "invalid require number");
         for(uint i=0; i<_owners.length; i++){
             address owner = _owners[i];
@@ -69,7 +80,7 @@ contract MultiSignWallet {
         emit Deposit(msg.sender, msg.value);
     }
 
-    function submit(address _to, uint _value, bytes32 _data) external onlyOwner() {
+    function submitTransaction(address _to, uint _value, bytes calldata _data) external onlyOwner() {
         transactions.push(Transaction({
             to: _to,
             value: _value,
@@ -79,7 +90,7 @@ contract MultiSignWallet {
         emit Submit(transactions.length - 1);
     }
 
-    function approves(uint txId) external onlyOwner() notApproved(txId) notExecuted(txId) txnExists(txId) {
+    function approvesTransaction(uint txId) external onlyOwner() notApproved(txId) notExecuted(txId) txnExists(txId) {
         approve[txId][msg.sender] = true;
         emit Approve(msg.sender, txId);
     }
@@ -92,16 +103,28 @@ contract MultiSignWallet {
         }
     }
 
-    function execute(uint txId) external notExecuted(txId) txnExists(txId) {
+    function executeTransaction(uint txId) external notExecuted(txId) txnExists(txId) {
         Transaction storage transaction = transactions[txId];
         transaction.executed = true;
-        (bool success, ) = transaction.to.call{value: transaction.value}(transaction.data);
+        (bool success, bytes memory returnData) = transaction.to.call{value: transaction.value}(transaction.data);
         require(success, "tx fail");
-        emit Execute(txId);
+        emit ExecuteTransaction(msg.sender, txId);
     }
 
-    function revoke(uint txId) external onlyOwner() notExecuted(txId) txnExists(txId){
+    function revokeTransaction(uint txId) external onlyOwner() notExecuted(txId) txnExists(txId){
         approve[txId][msg.sender] = false;
     }
 
+    function getTransaction(uint idx) public view returns (address to, uint value, bytes memory data, bool executed){
+        Transaction storage tran = transactions[idx];
+        return(tran.to, tran.value, tran.data, tran.executed);
+    }
+
+    function getTransactionCount() public view returns (uint){
+        return transactions.length;
+    }
+
+    function getOwnerCount() public view returns (uint){
+        return owners.length;
+    }
 }
